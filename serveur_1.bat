7beb5767d93c0f076c7b69e45409c412a0bc4964
Kurosx <Kurosx@users.noreply.github.com>
2017-07-12 20:19:11 +0200

    Add files via upload

---
+++ serveur_1.bat
@@ -0,0 +1,4 @@
+@echo off
+rem http://php.net/manual/fr/features.commandline.webserver.php
+rem Ce fichier est à placer à la racine du serveur (=document root)
+D:\apps\php7.1\php -S localhost:8403
---
+++ serveur_2.bat
@@ -0,0 +1,5 @@
+@echo off
+rem http://php.net/manual/fr/features.commandline.webserver.php
+rem rem Ce fichier est à placer dans le dossier parent du document root
+cd D:\apps\demo
+D:\apps\php5.5.14\php -S localhost:8201
